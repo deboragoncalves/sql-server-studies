c5080117c317108cb6cacd616c2cf41911182b9b
Debora Goncalves <Debora.Goncalves@senior.com.br>
2021-01-05 17:21:53 -0300

    inserir data - tabela livros

---
+++ insert/insert-book-date.sql
@@ -0,0 +1,7 @@
+USE book_store;
+USE book_store;
+GO
+
+-- Insert com date: 'YYYY-MM-DD'
+
+INSERT INTO [book] ([NAME], [AUTHOR], [CATEGORY], [DATE_CREATED]) VALUES ('Harry Potter', 'JK Rowlling', 'Ficção', '2002-10-31');
