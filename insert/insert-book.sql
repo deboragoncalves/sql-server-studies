43b1bcba218ad4611b6f2db4c75cbc36787da399
Debora Goncalves <Debora.Goncalves@senior.com.br>
2021-01-05 16:20:00 -0300

    inserts

---
+++ insert/insert-book.sql
@@ -0,0 +1,4 @@
+USE book_store;
+GO
+
+INSERT INTO [book] ([NAME], [AUTHOR], [CATEGORY]) VALUES ('Harry Potter', 'JK Rowlling', 'Ficção');
---
+++ insert/insert-client.sql
@@ -0,0 +1,4 @@
+USE book_store;
+GO
+
+INSERT INTO client ([CPF], [NAME], [PHONE_NUMBER], [FIRST_BUY]) VALUES ('00000000000', 'João da Silva', '(21) 91919-1919', 0);
